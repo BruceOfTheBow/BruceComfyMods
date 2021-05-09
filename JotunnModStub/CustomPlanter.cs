27d8fd1f0561edb100e768ae399c75395602f690
Bryan Smith <mugenman1111@gmail.com>
2021-05-09 15:18:13 -0500

    OK its named

---
+++ JotunnModStub/CustomPlanter.cs
@@ -0,0 +1,29 @@
+﻿// JotunnModStub
+// a Valheim mod skeleton using Jötunn
+// 
+// File:    JotunnModStub.cs
+// Project: JotunnModStub
+
+using BepInEx;
+using UnityEngine;
+using BepInEx.Configuration;
+using Jotunn.Utils;
+
+namespace CustomPlanter
+{
+    [BepInPlugin(PluginGUID, PluginName, PluginVersion)]
+    [BepInDependency(Jotunn.Main.ModGuid)]
+    internal class CustomPlanter : BaseUnityPlugin
+    {
+        public const string PluginGUID = "com.jotunn.CustomPlanter";
+        public const string PluginName = "CustomPlanter";
+        public const string PluginVersion = "0.0.1";
+
+        private void Awake()
+        {
+           
+
+        }
+
+    }
+}
