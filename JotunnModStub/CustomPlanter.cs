237c1ccb84a0a6ad441cf48e2cf4a4ce027b6641
Bryan Smith <mugenman1111@gmail.com>
2021-05-09 15:30:18 -0500

    OK theres like some plants

--- JotunnModStub/CustomPlanter.cs
+++ JotunnModStub/CustomPlanter.cs
@@ -7,6 +7,9 @@
 using BepInEx;
 using UnityEngine;
 using BepInEx.Configuration;
+using Jotunn.Configs;
+using Jotunn.Managers;
+using Jotunn.Entities;
 using Jotunn.Utils;
 
 namespace CustomPlanter
@@ -18,12 +21,63 @@ namespace CustomPlanter
         public const string PluginGUID = "com.jotunn.CustomPlanter";
         public const string PluginName = "CustomPlanter";
         public const string PluginVersion = "0.0.1";
+        private AssetBundle assetplanter;
+        private AssetBundle plants;
+
 
         private void Awake()
         {
-           
+
+            LoadAssets();
+            LoadTable();
+        }
+
+
+        private void LoadAssets()
+        {
+            assetplanter = AssetUtils.LoadAssetBundleFromResources("plantit", typeof(CustomPlanter).Assembly);
+            plants = AssetUtils.LoadAssetBundleFromResources("eightplants3", typeof(CustomPlanter).Assembly);
 
         }
 
+
+        private void LoadTable()
+        {
+            PieceManager.Instance.AddPieceTable(assetplanter.LoadAsset<GameObject>("_PlantitPieceTable"));
+            LoadShovel();
+
+        }
+
+        private void LoadShovel()
+        {
+            var shovelfab = assetplanter.LoadAsset<GameObject>("Planter");
+            var shovel = new CustomItem(shovelfab, fixReference: true,
+                new ItemConfig
+                {
+                    Amount = 1,
+                    Requirements = new[]
+                    {
+                        new RequirementConfig { Item = "Wood", Amount = 1}
+                    }
+                });
+            ItemManager.Instance.AddItem(shovel);
+
+        }
+
+        private void PlantMaker()
+        {
+            var planterthing = plants.LoadAsset<GameObject>("customitem_birdofparadise");
+            var planter = new CustomPiece(planterthing,
+                new PieceConfig
+                {
+                    PieceTable = "_PlantitPieceTable",
+                    AllowedInDungeons = false,
+                    Requirements = new[]
+                    {
+                        new RequirementConfig {Item = "Wood", Amount = 2, Recover = true}
+                    }
+                });
+            PieceManager.Instance.AddPiece(planter);
+        }
     }
 }
