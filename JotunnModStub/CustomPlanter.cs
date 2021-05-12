91d3295ac5b674182100367835a45e325f0ea9b8
DS <iliketurtles8448@gmail.com>
2021-05-11 22:14:47 -0400

    idk what im doing

--- JotunnModStub/CustomPlanter.cs
+++ JotunnModStub/CustomPlanter.cs
@@ -36,7 +36,7 @@ namespace CustomPlanter
         private void LoadAssets()
         {
             assetplanter = AssetUtils.LoadAssetBundleFromResources("plantit", typeof(CustomPlanter).Assembly);
-            plants = AssetUtils.LoadAssetBundleFromResources("eightplants3", typeof(CustomPlanter).Assembly);
+            plants = AssetUtils.LoadAssetBundleFromResources("custompiece_plantset1", typeof(CustomPlanter).Assembly);
 
         }
 
@@ -66,11 +66,11 @@ namespace CustomPlanter
 
 
         /// you will care about copying basically this chunk of code right here 
-        private void PlantMaker() //this is the function name you need to change it 
+        private void Bird_of_Paradise_Plant() //this is the function name you need to change it 
         {
-            var planterthing = plants.LoadAsset<GameObject>("customitem_birdofparadise"); //this defines the prefab name in your uniy project 
+            var Bird_of_Paradise_Thing = plants.LoadAsset<GameObject>("custompiece_birdofparadise"); //this defines the prefab name in your uniy project 
            // you need to change planterthing and planter every time you copy this 
-            var planter = new CustomPiece(planterthing,
+            var Bird_of_Paradise = new CustomPiece(Bird_of_Paradise_Thing,
                 new PieceConfig
                 {
                     PieceTable = "_PlantitPieceTable",
@@ -80,7 +80,43 @@ namespace CustomPlanter
                         new RequirementConfig {Item = "Wood", Amount = 2, Recover = true}
                     }
                 });
-            PieceManager.Instance.AddPiece(planter);
+            PieceManager.Instance.AddPiece(Bird_of_Paradise);
+        }
+
+
+        private void Bonsai_Plant() //this is the function name you need to change it 
+        {
+            var Bonsai_Thing = plants.LoadAsset<GameObject>("custompiece_bonsai"); //this defines the prefab name in your uniy project 
+                                                                                          // you need to change planterthing and planter every time you copy this 
+            var Bonsai = new CustomPiece(Bonsai_Thing,
+                new PieceConfig
+                {
+                    PieceTable = "_PlantitPieceTable",
+                    AllowedInDungeons = false,
+                    Requirements = new[]
+                    {
+                        new RequirementConfig {Item = "Wood", Amount = 2, Recover = true}
+                    }
+                });
+            PieceManager.Instance.AddPiece(Bonsai);
+        }
+
+
+        private void Barrel_Cactus_Plant() //this is the function name you need to change it 
+        {
+            var Barrel_Cactus_Thing = plants.LoadAsset<GameObject>("custompiece_barrelcactus"); //this defines the prefab name in your uniy project 
+                                                                                          // you need to change planterthing and planter every time you copy this 
+            var Barrel_Cactus = new CustomPiece(Barrel_Cactus_Thing,
+                new PieceConfig
+                {
+                    PieceTable = "_PlantitPieceTable",
+                    AllowedInDungeons = false,
+                    Requirements = new[]
+                    {
+                        new RequirementConfig {Item = "Wood", Amount = 2, Recover = true}
+                    }
+                });
+            PieceManager.Instance.AddPiece(Barrel_Cactus);
         }
 
         // to here 
