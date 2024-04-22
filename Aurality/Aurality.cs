39870ff8cd6a8b4fc90a8ffb8e9e4fcf14373b9a
redseiko <redseiko@users.noreply.github.com>
2024-04-21 17:18:53 -0700

    Update Aurality to `v1.1.0`.
    * Fixed for the `v0.217.46` patch.
    * Moved changelog into `CHANGELOG.md`.
    * Bumped up `<LangVersion>` to C# 10.
    * Code clean-up and refactoring.

--- Aurality/Aurality.cs
+++ Aurality/Aurality.cs
@@ -1,34 +1,28 @@
-﻿using BepInEx;
+﻿namespace Aurality;
+
+using BepInEx;
 
 using HarmonyLib;
 
-using UnityEngine;
-
-using System.Collections;
-using System.Collections.Generic;
 using System.Reflection;
 
-using static Aurality.PluginConfig;
+using static PluginConfig;
 
-namespace Aurality {
-  [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
-  public class Aurality : BaseUnityPlugin {
-    public const string PluginGuid = "bruce.comfy.valheim.aurality";
-    public const string PluginName = "Aurality";
-    public const string PluginVersion = "1.0.1";
+[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+public sealed class Aurality : BaseUnityPlugin {
+  public const string PluginGuid = "bruce.comfy.valheim.aurality";
+  public const string PluginName = "Aurality";
+  public const string PluginVersion = "1.1.0";
 
-    Harmony _harmony;
+  Harmony _harmony;
 
-    public static readonly List<string> BuzzPrefabs = new List<string> { "sfx_deathsquito_attack(Clone)" };
+  void Awake() {
+    BindConfig(Config);    
 
-    public void Awake() {
-      BindConfig(Config);    
+    _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGuid);
+  }
 
-      _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), harmonyInstanceId: PluginGuid);
-    }
-
-    public void OnDestroy() {
-      _harmony?.UnpatchSelf();
-    }
+  void OnDestroy() {
+    _harmony?.UnpatchSelf();
   }
 }
